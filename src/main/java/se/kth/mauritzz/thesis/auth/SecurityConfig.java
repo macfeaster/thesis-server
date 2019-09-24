0fd9b93dbe7020fad988fe0f76a032e09d8e6bdb
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-09-24 14:53:02 +0200

    Security config fixes

--- src/main/java/se/kth/mauritzz/thesis/auth/SecurityConfig.java
+++ src/main/java/se/kth/mauritzz/thesis/auth/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/create", "/welcome", "/css/**").permitAll()
+                    .antMatchers("/create", "/css/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
